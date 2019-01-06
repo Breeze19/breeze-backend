ca16a1ccb6e3ff3b086981ec5353ff68ddd3f7d5
sd1998 <shashvat51@gmail.com>
2019-01-06 23:18:54 +0530

    Messages added

--- Breeze/static/assets/js/index.js
+++ Breeze/static/assets/js/index.js
@@ -18,7 +18,8 @@ $(document).ready(function(){
     }
     $.post("https://breeze19sports.herokuapp.com/register",{data},function(response){
       if(response.result == 'OK'){
-        
+        $("#message").removeAttr('hidden')
+        $("#message").text("Registration successful")
       }
     })
   })
@@ -34,7 +35,8 @@ $(document).ready(function(){
     }
     $.post("https://breeze19sports.herokuapp.com/register/tkk",{data},function(response){
       if(response.result == 'OK'){
-        
+        $("#message").removeAttr('hidden')
+        $("#message").text("Registration successful")
       }
     })
   })
@@ -51,7 +53,8 @@ $(document).ready(function(){
     }
     $.post("https://breeze19sports.herokuapp.com/register/tkp",{data},function(response){
       if(response.result == 'OK'){
-        
+        $("#message").removeAttr('hidden')
+        $("#message").text("Registration successful")
       }
     })
   })
